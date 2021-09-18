7f80957ceb4317b73b4fc81a69fe6603bc6afd59
Leen Barakat <leenbarakat2432001@outlook.com>
2021-09-18 15:07:39 -0700

    add lab3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import { React, Component } from "react";
-import Header from './Components/Header';
-import Main from './Components/Main';
-import Footer from './Components/Footer';
-import data from './data.json';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
+import SelectedBeast from "./Components/SelectedBeast ";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      animalData: data,
+      showModal: false,
+      name: '',
+      image_url: '',
+      description:'',
     }
   }
+  handleClose = () => {
+    this.setState({
+      showModal: false
+    });
+  }
+  handleOpen = (name, image_url,description) => {
+    this.setState({
+      showModal: true,
+      name: name,
+      image_url: image_url,
+      description: description,
+    });
+  }
+
   render() {
     return (
       <>
         <Header />
-        <Main animalData={this.state.animalData}/>
-        <Footer />
+        <Main handleOpen={this.handleOpen} />
+        <SelectedBeast
+          handleClose={this.handleClose}
+          showModal={this.state.showModal}
+          name={this.state.name}
+          image_url={this.state.image_url}
+          description={this.state.description}
+        />
+        <Footer/>
       </>
     )
   }
--- src/component/hornedbeasts.js
+++ src/component/hornedbeasts.js
@@ -9,17 +9,22 @@ class HornedBeast extends Component {
     }
   }
   vote = () => {
+    this.props.handleOpen(this.props.title, this.props.image_url, this.props.description);
     this.setState({
       numVotes: this.state.numVotes + 1,
       likes: <p>&#10084;</p>,
     })
   }
   render() {
+    const styling = {
+      width: '400px',
+      height: '300px',
+    }
     return (
       <>
         <h2>title: {this.props.title}</h2>
         <h3>keyword: {this.props.keyword}</h3>
-        <img onClick={this.vote} src={this.props.image_url} alt=""></img>
+        <img style={styling} onClick={this.vote} src={this.props.image_url} alt=""></img>
         <p>votes: {this.state.numVotes}</p>
         <div>{this.state.likes}</div>
         <p>description: {this.props.description}</p>
