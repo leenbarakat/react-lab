e7f0763d1c5d91c79d9a33e42c6ff90e276dd650
Leen Barakat <leenbarakat2432001@outlook.com>
2021-09-18 14:51:21 -0700

    add lab2

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 import { React, Component } from "react";
 import Header from './Components/Header';
 import Main from './Components/Main';
 import Footer from './Components/Footer';
+import data from './data.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-);}
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      animalData: data,
+    }
+  }
   render() {
     return (
       <>
         <Header />
-        <Main/>
+        <Main animalData={this.state.animalData}/>
         <Footer />
       </>
     )
